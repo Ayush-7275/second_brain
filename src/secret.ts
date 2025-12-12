a98bca3732cf05e0cab7297d767a974664d3d9d7
Ayush-7275 <ayush7275748400@gmail.com>
2025-12-12 20:34:56 +0530

    stopped tracking secret

--- src/secret.ts
+++
@@ -1,4 +0,0 @@
-export const mongoPasswd =
-    "mongodb+srv://ayush7275:DRVrgzlKwu1uOFBu@secondbrain.dexuqtv.mongodb.net/?appName=SecondBrain";
-export const JWT_PASSWORD = "erling";
-export const HASHINGPASSWD = "halland";
